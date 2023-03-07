90c1cf5dec49bfe2a462586e15e972ddc44b28e7
dominikmuller <d.mueller@emplify-software.de>
2023-03-07 18:53:23 +0100

    Add more tests

---
+++ tests/Feature/EntriesFieldtypeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Doefom\Restrict\Tests\Feature;
+
+use Doefom\Restrict\Fieldtypes\Entries;
+use Doefom\Restrict\Tests\TestCase;
+use Illuminate\Support\Facades\Request;
+
+class EntriesFieldtypeTest extends TestCase
+{
+
+    /** @test */
+    public function user_a_can_see_other_authors_entries_of_collection_a()
+    {
+        $this->actingAs($this->userA);
+        $request = Request::create('/cp/fieldtypes/relationship');
+        $this->assertEquals(2, (new Entries())->getIndexQuery($request)->count());
+    }
+
+    /** @test */
+    public function user_b_cannot_see_other_authors_entries_of_collection_a()
+    {
+        $this->actingAs($this->userB);
+        $request = Request::create('/cp/fieldtypes/relationship');
+        $this->assertEquals(1, (new Entries())->getIndexQuery($request)->count());
+    }
+
+}
